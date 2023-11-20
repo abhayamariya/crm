c297b93eaaf098068d8c6c4a3a6051607c010a63
abhaya <abhayachu23@gmail.com>
2023-11-20 21:08:31 +0530

    initial commit

---
+++ crm/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Employees(models.Model):
+    name=models.CharField(max_length=200)
+    department=models.CharField(max_length=200)
+    salary=models.PositiveIntegerField()
+    email=models.EmailField(unique=True)
+    contact=models.CharField(max_length=10,null=True)
+    age=models.PositiveIntegerField()
+    profile_pic=models.ImageField(upload_to="images",null=True,blank=True)
+    dob=models.DateField(null=True,blank=True)
+
+
+    def __str__(self):
+        return self.name
+
